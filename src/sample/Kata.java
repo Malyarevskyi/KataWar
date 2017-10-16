6ae1f4c8200ec41dd14a8b733c1b4fb9747a89e8
Toxa <pochta13a@gmail.com>
2017-10-17 00:27:41 +0300

    +kata

--- src/sample/Kata.java
+++ src/sample/Kata.java
@@ -1,4 +1,5 @@
 package sample;
+
 import java.util.Arrays;
 
 public class Kata {
@@ -18,33 +19,63 @@ public class Kata {
     }
 
     public static int[] squareOrSquareRoot(int[] array) {
-       return Arrays.stream(array).map(i -> Math.sqrt(i) % 1 == 0 ? ((int) Math.sqrt(i)): (i*i)).toArray();
+        return Arrays.stream(array).map(i -> Math.sqrt(i) % 1 == 0 ? ((int) Math.sqrt(i)) : (i * i)).toArray();
     }
 
     public static String removeExclamationMarks(String s) {
-        return s.replaceAll("!","");
+        return s.replaceAll("!", "");
     }
 
-    public static int[] countPositivesSumNegatives(int[] input)
-    {   int [] result = new int[2];
-        boolean empty = true;
-        for (int i=0; i<input.length; i++) {
-            if (null != input[i]) {
-                empty = false;
-                break;
-            }}
-
-
-        if (input == null)
-        { result = new int[0];
-            return result;
+    public static int[] countPositivesSumNegatives(int[] input) {
+        if (input == null || input.length == 0) return new int[]{};
+        int sum = 0, count = 0;
+        for (int i : input) {
+            if (i > 0) count++;
+            else sum += i;
         }
-        for (int i = 0; i < input.length; i++)
-        {if (input[i]>0){
-
-            result[0]+=1;}
-        else result[1]+=input[i];
-        }
-        return result;
+        return new int[]{count, sum};
     }
+
+    public static long stairsIn20(int[][] stairs) {
+        return 20 * Arrays.stream(stairs).flatMapToInt(arr -> Arrays.stream(arr)).sum();
+    }
+
+//    public static long stairsIn20(int[][] stairs){
+//
+//        int total = 0;
+//        for(int i = 0; i < stairs.length; i++){
+//            for(int j = 0; j < stairs[1].length; j++){
+//                total = total + stairs[i][j];
+//            }
+//        }
+//        total = total * 20;
+//        return total;
+//    }
+
+    public static int[] digitize(long n) {
+        return new StringBuilder().append(n)
+                .reverse()
+                .chars()
+                .map(Character::getNumericValue)
+                .toArray();
+    }
+
+
+    public static String position(char alphabet) {
+        String text = "Position of alphabet: ";
+        String base = "0abcdefghijklmnopqrstuvwxyz";
+        for (char symbolChar : base.toCharArray()) {
+            if (symbolChar == alphabet)
+                return text + Integer.toString(base.indexOf(symbolChar));
+        }
+
+        return text + Integer.toString(0);
+    }
+
+//    public static String position(char alphabet)
+//    {
+//        return "Position of alphabet: " + ((int) alphabet - 96);
+//    }
+
+
 }
